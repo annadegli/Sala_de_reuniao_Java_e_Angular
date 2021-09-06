9ea19553c8a972dfa70a4ee36713b5f1bd0cee5a
annadegli <annaclaradegliomini@hotmail.com>
2021-09-06 16:16:06 -0300

    Second commit - create archive model, new dependencies include

--- src/main/java/com/digital/crud/saladereuniao/SaladereuniaoApplication.java
+++ src/main/java/com/digital/crud/saladereuniao/SaladereuniaoApplication.java
@@ -3,11 +3,13 @@ package com.digital.crud.saladereuniao;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import static org.springframework.boot.SpringApplication.*;
+
 @SpringBootApplication
 public class SaladereuniaoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SaladereuniaoApplication.class, args);
+		run(SaladereuniaoApplication.class, args);
 	}
 
 }
